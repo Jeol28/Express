778d9a7a3ef1a67d1d1ca2a9f985c4cc794f8a40
Jeol28 <jorgeolaya2803@gmail.com>
2026-03-25 00:32:24 -0500

    Routes usuario

---
+++ src/controller/users.controller.js
@@ -0,0 +1,34 @@
+import { User } from "../models/Users.js";
+
+export const getUsers = async (req, res) => {
+    const users = await User.findAll();
+    return res.json(users);
+}
+
+export const createUser = async (req, res) => {
+    const newUser = await User.create(req.body);
+    return res.json(newUser);
+}
+
+export const updateUser = async (req, res) => {
+    const id = req.params.id;
+    const user = await User.findByPk(id);
+    try{
+        await user.update(req.body);
+    } catch (error) {
+        console.log(error);
+    }
+
+    return res.json(user);
+}
+
+export const deleteUser = async (req, res) => {
+    const id = req.params.id;
+    const user = await User.findByPk(id);
+    try{
+        await user.destroy();
+    } catch (error) {
+        console.log(error);
+    }
+    return res.sendStatus(204);
+}
